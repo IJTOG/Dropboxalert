e02efe81e2f99e87caf657a05376ca3c439b66d5
gotzeallaz <rattawut@hotmail.com>
2019-11-10 00:26:03 +0700

    update port

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,15 @@ import path from "path";
 
 // port is now available to the Node.js runtime
 // as if it were an environment variable
-const port = process.env.SERVER_PORT || 3000;
+// const port = process.env.SERVER_PORT || 3000;
+const port = 80;
 
 const setupRoutes = (App: express.Application) => {
   const APP_DIR = `${__dirname}/component`;
   const features = fs
     .readdirSync(APP_DIR)
-    .filter((file) => fs.statSync(`${APP_DIR}/${file}`).isDirectory());
-  features.forEach((feature) => {
+    .filter(file => fs.statSync(`${APP_DIR}/${file}`).isDirectory());
+  features.forEach(feature => {
     const router = express.Router();
     const routes = require(`${APP_DIR}/${feature}/routes`);
     routes.setup(router);
