91bcec3ebeda93fcbf20b4191c9f4db5a3a85dfb
gotzeallaz <rattawut@hotmail.com>
2019-11-11 11:33:57 +0700

    test push

--- src/component/line/controller.ts
+++ src/component/line/controller.ts
@@ -5,13 +5,14 @@ const userController = {
     res.send("reply bot");
   },
   async webhook(req, res) {
+    console.log(req.body.events[0].source.userId);
     let token = req.body.events[0].replyToken;
     let headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer 3ip7P+o9dJjibvEcELBCfN5WQYH9EXyLgzK+qW5GXuA6b2MGHdlwRD8P8hom5LZd70CAd0AnhBhYtiKZGle1fcnaraSSfkEwGMqGh3vlfeN3firzOa9PSf8anFoMEfXgMtiuh55kYoUpOwFePCUSXgdB04t89/1O/w1cDnyilFU=`
     };
     let data = {
-      replyToken: token,
+      to: req.body.events[0].source.userId,
       messages: [
         {
           type: "text",
@@ -19,10 +20,22 @@ const userController = {
         }
       ]
     };
+    // let data = {
+    //   replyToken: token,
+    //   messages: [
+    //     {
+    //       type: "text",
+    //       text: "เรียกหาพ่องงงงง"
+    //     }
+    //   ]
+    // };
     try {
-      await axios.post("https://api.line.me/v2/bot/message/reply", data, {
+      await axios.post("https://api.line.me/v2/bot/message/push", data, {
         headers
       });
+      // await axios.post("https://api.line.me/v2/bot/message/reply", data, {
+      //   headers
+      // });
     } catch (err) {
       console.log(err);
     }
